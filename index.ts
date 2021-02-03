26bd860d77248501d221c282803afc821a369e85
Anwesh43 <anweshthecool0@gmail.com>
2021-02-04 02:24:44 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 3 
+const balls : number = 3 
 const scGap : number = 0.02 / parts 
 const strokeFactor : number = 90 
 const sizeFactor : number = 9.9 
@@ -26,5 +27,48 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawDotToEn(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const r : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < balls; j++) {
+            context.save()
+            context.translate(-2 * r + 2 * r * j, 0)
+            const x : number = (2 * r - 2 * r * j) * sf2
+            const y : number = (h * 0.5 - r) * sf2
+            DrawingUtil.drawCircle(context, x, y,  r * sf1)
+            DrawingUtil.drawLine(context, 0, 0, x, y)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawDTENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawDTENode(context, i, scale)
     }
 }
